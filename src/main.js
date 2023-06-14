731b8c16b2ab9f9912eacbb04a0b5d11ecf074a5
Kieran Czerwinski <kierwinski@gmail.com>
2023-06-14 00:51:45 -0700

    feat: design overhaul

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log(`Hi there!`);
+const cursor = document.querySelector('.cursor');
+
+const X_OFFSET = 50;
+const Y_OFFSET = 50;
+
+const moveCursor = (e) => {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+
+  cursor.style.transform = `translate3d(${mouseX - X_OFFSET}px, ${mouseY - Y_OFFSET}px, 0)`;
+}
+
+window.addEventListener('mousemove', moveCursor);
