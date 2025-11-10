781395ef3108127cb674589d7ccf610add8f48fb
Kieran Czerwinski <kierwinski@gmail.com>
2025-11-10 00:30:42 -0800

    fix: safari fixes

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 const cursor = document.querySelector('.cursor');
 
-const X_OFFSET = 50;
-const Y_OFFSET = 50;
+const X_OFFSET = 50; 
+const Y_OFFSET = 50; 
 
 const moveCursor = (e) => {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;
+  let clientX, clientY;
 
-  cursor.style.transform = `translate3d(${mouseX - X_OFFSET}px, ${mouseY - Y_OFFSET}px, 0)`;
+  // Handle touch events (for iOS)
+  if (e.touches && e.touches.length) {
+    clientX = e.touches[0].clientX;
+    clientY = e.touches[0].clientY;
+  } 
+  else {
+    clientX = e.clientX;
+    clientY = e.clientY;
+  }
+
+  cursor.style.transform = `translate3d(${clientX - X_OFFSET}px, ${clientY - Y_OFFSET}px, 0)`;
 }
 
 window.addEventListener('mousemove', moveCursor);
+window.addEventListener('touchmove', moveCursor);
