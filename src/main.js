0fe4afd73f865efbd2d3915a7e7316237907ab2c
Kieran Czerwinski <kierwinski@gmail.com>
2025-11-10 01:31:42 -0800

    feat: create blob on press

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-const cursor = document.querySelector('.cursor');
+const BLOB_CLASS = 'blob'; 
+const X_OFFSET = 50;
+const Y_OFFSET = 50;
 
-const X_OFFSET = 50; 
-const Y_OFFSET = 50; 
+const createBlob = (clientX, clientY) => {
+  const newBlob = document.createElement('div');
+  newBlob.classList.add(BLOB_CLASS);
+  document.querySelector('.filter-container').appendChild(newBlob);
+  
+  newBlob.style.transform = `translate3d(${clientX - X_OFFSET}px, ${clientY - Y_OFFSET}px, 0)`;
+};
 
-const moveCursor = (e) => {
+
+const handlePress = (e, shouldCreateBlob = true) => {
   let clientX, clientY;
 
-  // Handle touch events (for iOS)
   if (e.touches && e.touches.length) {
     clientX = e.touches[0].clientX;
     clientY = e.touches[0].clientY;
@@ -15,9 +22,13 @@ const moveCursor = (e) => {
     clientX = e.clientX;
     clientY = e.clientY;
   }
+  
+  if (shouldCreateBlob) {
+    createBlob(clientX, clientY);
+  }
+  
+};
 
-  cursor.style.transform = `translate3d(${clientX - X_OFFSET}px, ${clientY - Y_OFFSET}px, 0)`;
-}
-
-window.addEventListener('mousemove', moveCursor);
-window.addEventListener('touchmove', moveCursor);
+window.addEventListener('mousedown', handlePress);
+window.addEventListener('touchstart', handlePress, { passive: true });
+window.addEventListener('touchmove', (e) => handlePress(e, false));
